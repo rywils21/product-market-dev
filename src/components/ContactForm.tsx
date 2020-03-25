00f0e80a5569975422a10172dacf0240a20d6dc4
Ryan Wilson <rywils21@gmail.com>
2020-03-24 21:20:48 -0500

    initial bit of contact form

---
+++ src/components/ContactForm.tsx
@@ -0,0 +1,23 @@
+import React, { useState } from "react";
+import { useForm, ValidationError } from "@statickit/react";
+
+export function ContactForm(props) {
+  const [state, handleSubmit] = useForm("contactForm");
+
+  if (state.succeeded) {
+    return <p>Thanks for joining!</p>;
+  }
+
+  return (
+    <form onSubmit={handleSubmit}>
+      <label htmlFor="email">Email Address</label>
+      <input id="email" type="email" name="email" />
+      <ValidationError prefix="Email" field="email" errors={state.errors} />
+      <textarea id="message" name="message" />
+      <ValidationError prefix="Message" field="message" errors={state.errors} />
+      <button type="submit" disabled={state.submitting}>
+        Submit
+      </button>
+    </form>
+  );
+}
--- src/components/Layout.tsx
+++ src/components/Layout.tsx
@@ -78,18 +78,6 @@ const routeConfig: RouteItem[] = [
     )
   },
   {
-    displayName: "Team",
-    route: "/team",
-    icon: (
-      <path
-        strokeLinecap="round"
-        strokeLinejoin="round"
-        strokeWidth="2"
-        d="M17 20h5v-2a3 3 0 00-5.356-1.857M17 20H7m10 0v-2c0-.656-.126-1.283-.356-1.857M7 20H2v-2a3 3 0 015.356-1.857M7 20v-2c0-.656.126-1.283.356-1.857m0 0a5.002 5.002 0 019.288 0M15 7a3 3 0 11-6 0 3 3 0 016 0zm6 3a2 2 0 11-4 0 2 2 0 014 0zM7 10a2 2 0 11-4 0 2 2 0 014 0z"
-      />
-    )
-  },
-  {
     displayName: "Metrics",
     route: "/metrics",
     icon: (
@@ -157,9 +145,11 @@ function DesktopNav() {
                 <p className="text-sm leading-5 font-medium text-white">
                   By Ryan Wilson
                 </p>
-                <p className="text-xs leading-4 font-medium text-gray-400 group-hover:text-gray-300 group-focus:underline transition ease-in-out duration-150">
-                  View on Twitter
-                </p>
+                <Link href="/contact">
+                  <p className="text-xs leading-4 font-medium text-gray-400 group-hover:text-gray-300 group-focus:underline transition ease-in-out duration-150">
+                    Contact Me
+                  </p>
+                </Link>
               </div>
             </div>
           </a>
@@ -264,9 +254,11 @@ function MobileNav({ sidebarOpen, setSidebarOpen }: MobileNavPropTypes) {
                 <p className="text-base leading-6 font-medium text-white">
                   By Ryan Wilson
                 </p>
-                <p className="text-sm leading-5 font-medium text-gray-400 group-hover:text-gray-300 transition ease-in-out duration-150">
-                  View on Twitter
-                </p>
+                <Link href="/contact">
+                  <p className="text-sm leading-5 font-medium text-gray-400 group-hover:text-gray-300 transition ease-in-out duration-150">
+                    Contact Me
+                  </p>
+                </Link>
               </div>
             </div>
           </a>
